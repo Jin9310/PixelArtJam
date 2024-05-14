41fd834588695605aac90ab207fac0c9b50d662b
Jindrich <stejskalj@hotmail.com>
2024-05-14 19:01:48 +0200

    player position in the tube

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,14 +6,20 @@ extends CharacterBody2D
 var center := Vector2(576/2, 324/2)  # Center of the circle
 var radius = 25  # Radius of the circle
 
+var on_right_side: bool = false
+var on_left_side: bool = false
+
 # Character parameters
 var angle = PI/2  # Initial angle at the bottom of the circle
 var angular_speed = 2  # Angular speed of movement
 var min_angle = PI/6  # Minimum angle on the left side (30 degrees)
 var max_angle = 5*PI/6  # Maximum angle on the right side (150 degrees)
 
+var initial_angle
+
 func _ready():
 	tube.connect("show_feet", show_feet)
+	initial_angle = angle
 
 func _physics_process(delta):
 	# Handle player input for movement
@@ -29,6 +35,13 @@ func _physics_process(delta):
 	
 	# Update character's position
 	global_position = position_on_circle
+	
+	if angle > initial_angle:
+		on_left_side = true
+		on_right_side = false
+	if angle < initial_angle:
+		on_left_side = false
+		on_right_side = true
 	
 	# Rotate the sprite
 	%Sprite2D.rotation = angle - PI/2  # Adjust rotation to match character's orientation
